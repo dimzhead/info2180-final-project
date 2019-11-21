21227be29082786c98332ad0e4980b8ddb138af8
bastic18 <48740223+bastic18@users.noreply.github.com>
2019-11-21 04:05:32 -0500

    addded javascript

---
+++ createUser.js
@@ -0,0 +1,32 @@
+
+
+        
+
+$(document).ready(function () {
+    let srchBtn = $('#submit');
+ 
+
+    srchBtn.on('click', function(e){
+        e.preventDefault();
+        let fname= $("#firstname").val();
+let lname= $("#lastname").val();
+let email= $("#email").val();
+let password= $("#password").val();
+
+        $.ajax("createUser.php", {
+            method: "POST",
+            data: {
+                firstname:  fname,
+                lastname: lname,
+                email:email,
+                password:password
+
+            }        
+        }).done(function(response){
+            alert("successfully sent to the database to be added")
+        }).fail(function() {
+            alert("An error occured.");
+        });
+    });
+    
+});
